66f79d42d7d2cc2bf588bf6f9cd9973b3cd6c7be
a.fedoseev <a.fedoseev@corp.mail.ru>
2021-01-15 13:03:48 +0300

    test

--- test/specs/basic.ts
+++ test/specs/basic.ts
@@ -1,7 +1,6 @@
 describe('webdriver.io page', () => {
     it('should have the right title', () => {
-        browser.url('https://webdriver.io')
-        console.log(browser.getUrl());
-        expect(browser).toHaveTitle('WebdriverIO · Next-gen browser and mobile automation test framework for Node.js');
+        browser.url('https://webdriver.io');
+        throw new Error('Test');
     })
 })
--- wdio.conf.js
+++ wdio.conf.js
@@ -1,3 +1,29 @@
+// const reportportal = require('wdio-reportportal-reporter');
+// const RpService = require("wdio-reportportal-service");
+const RerunService = require('wdio-rerun-service');
+
+// const conf = {
+//   reportPortalClientConfig: { // report portal settings
+//     token: '0c7ee146-fc49-4420-9fbe-94cefd34ec30',
+//     endpoint: 'http://localhost:8080/api/v1',
+//     launch: 'superadmin_TEST_EXAMPLE',
+//     project: 'superadmin_personal',
+//     mode: 'DEFAULT',
+//     debug: false,
+//     description: "Описание проекта",
+//     attributes: [{key:"tag", value: "foo"}],
+//     headers: {"foo": "bar"} // optional headers for internal http client
+//   },
+//   reportSeleniumCommands: false, // add selenium commands to log
+//   seleniumCommandsLogLevel: 'debug', // log level for selenium commands
+//   autoAttachScreenshots: true, // automatically add screenshots
+//   screenshotsLogLevel: 'info', // log level for screenshots
+//   parseTagsFromTestTitle: false, // parse strings like `@foo` from titles and add to Report Portal
+//   cucumberNestedSteps: false, // report cucumber steps as Report Portal steps
+//   autoAttachCucumberFeatureToScenario: false, // requires cucumberNestedSteps to be true for use
+//   isSauseLabRun: false // automatically add SauseLab ID to rp tags.
+// };
+
 exports.config = {
     runner: 'local',
     specs: [
@@ -15,9 +41,17 @@ exports.config = {
     waitforTimeout: 10000,
     connectionRetryTimeout: 120000,
     connectionRetryCount: 3,
-    services: ['chromedriver'],
+    services: [
+        'chromedriver',
+        [RerunService],
+        // [RpService, {}]
+    ],
     framework: 'mocha',
-    reporters: ['spec',['allure', {outputDir: 'allure-results'}]],
+    reporters: [
+        'spec',
+        ['allure', {outputDir: 'allure-results'}], 
+        // [reportportal, conf]
+    ],
 
     mochaOpts: {
         // TypeScript setup
